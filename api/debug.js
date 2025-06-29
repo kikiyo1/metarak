10be160d20bc7119eac992a8005cea08d27783f6
kikiyo1 <bocahbagus168@gmail.com>
2025-06-29 22:23:29 +0700

    Update debug.js

--- api/debug.js
+++ api/debug.js
@@ -11,7 +11,8 @@ export default async function handler(req, res) {
   const r = await fetch('https://api.mayar.id/v1/payment_links', {
     method: 'POST',
     headers: {
-      'Authorization': `Bearer ${eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiIwZTg5YTQ1My03YzQ0LTQzZTYtYmY1ZS1hOGU1MzY4N2M1NzQiLCJhY2NvdW50SWQiOiI2ZDQzZDhiMC1kNGVkLTRkMmItOGQwMS1kMmIyZWI4YzJjOWIiLCJjcmVhdGVkQXQiOiIxNzUxMjA2MjM5NDgzIiwicm9sZSI6ImRldmVsb3BlciIsInN1YiI6InNpc3dvcm9tZXRhMTY4QGdtYWlsLmNvbSIsIm5hbWUiOiJyYWsgbWluaW1hcmtldCIsImxpbmsiOiJzaXN3b3JvLTc0Mzk3IiwiaXNTZWxmRG9tYWluIjpudWxsLCJpYXQiOjE3NTEyMDYyMzl9.S0Ra8vRbe5bCsXr7xJk01OvpPVB-vdGO1zSXl5MAVkw-eE4D6w706KbpjlmkFscbosQIzklxY22_3QdBsJHvuAxvSYbAzp6Xfjk3c71imziI8podfUahSW5TJHviMySzjkWzNzsdarxH9raFji6wbctLp9XXfuDUnTv6t49p9k6yAvkHd1Sl20j1FOF4xTUdfIb8oCY51knVSFDE0RsckGJ1Dw8XzYNHxnU6e4TfStfArkVgGUe2DX_dJQfHc2J85zZIQn8Qec9GaIJEV4x6pAXGpeA262kyWRUPVsiVRAWrDeB_3BtlnPtBdNA2bbjmcH2vFIerNS6H0pkg3kBBWw}`,
+     'Authorization': `Bearer ${MAYAR_API_KEY}`,
+
       'Content-Type': 'application/json',
     },
     body: JSON.stringify(testPayload),
